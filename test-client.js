1eb604b310220763e31ce0088ead369da6c4aeb7
PEZ <pez@pezius.com>
2026-06-28 13:54:25 +0200

    Remove non-standard isErrror in responses

--- test-client.js
+++ test-client.js
@@ -1,9 +1,9 @@
 const net = require('net');
 const client = new net.Socket();
 
-client.connect(1664, '127.0.0.1', () => {
+client.connect(55914, '127.0.0.1', () => {
     console.log('Connected');
-    client.write('{"jsonrpc":"2.0","id":4,"method":"tools/call","params":{"name":"clojure_evaluate_code","arguments":{"code":"(+ 1 2)","namespace":"user","replSessionKey":"joyride","who":"copilot"}}}\n');
+    client.write('{"method":"tools/list","params":{},"jsonrpc":"2.0","id":1}\n');
 });
 
 client.on('data', (data) => {
